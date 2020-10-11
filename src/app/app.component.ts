115d592a6713d2bb33d908897ff09ba55b1b33c8
Jesse Buitenhuis <jessebuitenhuis@Jesses-MacBook-Pro.local>
2020-10-11 17:13:19 +0200

    add alerts for serviceworker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'pwa';
+
+  constructor(swUpdate: SwUpdate) {
+    swUpdate.available.subscribe((event) => {
+      alert(
+        `Version ${event.available} available. Current is ${event.current}`
+      );
+    });
+
+    swUpdate.activated.subscribe((event) => {
+      alert(
+        `Version ${event.current} activated. Previous was ${event.previous}`
+      );
+    });
+  }
 }
